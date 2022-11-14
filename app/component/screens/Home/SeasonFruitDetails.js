010d4b0afdab0cc4bd6e2f96c43529ef6fdbc079
Akshay <akshay@Akshays-MacBook-Air.local>
2022-11-14 13:07:04 +0530

    updating fontsize

--- app/component/screens/Home/SeasonFruitDetails.js
+++ app/component/screens/Home/SeasonFruitDetails.js
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
   sectionSubText: {
     color: '#33425B',
     fontFamily: 'Cochin',
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "500",
     padding: 3
   },
